9d4a5ce18224fb4db8d6224f8614418edc99cfb9
massimilianoC <massimiliano.camillucci@gmail.com>
2024-03-22 16:27:05 +0100

    invert

--- scripts/cube-face.js
+++ scripts/cube-face.js
@@ -19,7 +19,7 @@ AFRAME.registerComponent('cube-face',{
                     newPlane.setAttribute('material','side: double');
                     newPlane.setAttribute('position',position);
                     newPlane.setAttribute('rotation',rotation);
-                    newPlane.setAttribute('src','#opera'+(c+p+2));
+                    newPlane.setAttribute('src','#opera'+(8-c+p));
                     newBlock.appendChild(newPlane);
                     console.log(newPlane);
                 }           
