24496f5ae721a1f497ba2e443d68a7ddf4bd2bbb
massimilianoC <massimiliano.camillucci@gmail.com>
2024-03-22 16:28:58 +0100

    hf

--- scripts/cube-face.js
+++ scripts/cube-face.js
@@ -19,7 +19,7 @@ AFRAME.registerComponent('cube-face',{
                     newPlane.setAttribute('material','side: double');
                     newPlane.setAttribute('position',position);
                     newPlane.setAttribute('rotation',rotation);
-                    newPlane.setAttribute('src','#opera'+(8-c+p));
+                    newPlane.setAttribute('src','#opera'+(8-c+p+i*2)%8);
                     newBlock.appendChild(newPlane);
                     console.log(newPlane);
                 }           
