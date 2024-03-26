5efa7168eb14c3664b9b81e3055f3be2038e14e8
massimilianoC <massimiliano.camillucci@gmail.com>
2024-03-26 14:49:26 +0100

    index

--- scripts/posterGallery.js
+++ scripts/posterGallery.js
@@ -67,10 +67,10 @@ document.addEventListener("DOMContentLoaded", function() {
   function buildGallery(parent,tag,itemCount){
     for (let index = 0; index < itemCount; index++) {
       var item = document.createElement('a-entity');
-      item.setAttribute("id",tag+"-item"+itemCount);
+      item.setAttribute("id",tag+"-item"+index);
       var plane = document.createElement('a-plane');
       plane.setAttribute("class","clickable");
-      plane.setAttribute("src","#"+tag+itemCount);
+      plane.setAttribute("src","#"+tag+index);
       plane.setAttribute("position","0 0 0");
       plane.setAttribute("height","1");
       plane.setAttribute("width","0.7");
