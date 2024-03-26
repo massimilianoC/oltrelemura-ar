576ebcb261864b58dbee3abea69dea3a7c6b2c54
massimilianoC <massimiliano.camillucci@gmail.com>
2024-03-26 09:23:35 +0100

    swap

--- scripts/rubik-cube.js
+++ scripts/rubik-cube.js
@@ -52,14 +52,14 @@ AFRAME.registerComponent('rubik-cube',{
                      //LEFT
                     else if(j==0 && axis==0 && direction==0) 
                     {
-                      newPlane.setAttribute('material','shader:flat ; src: url(../../../assets/texture/tileA_'+r+'_'+i+'.png)');
+                      newPlane.setAttribute('material','shader:flat ; src: url(../../../assets/texture/tileA_'+i+'_'+r+'.png)');
                       rotation = "0 -90 90";
                     }
 
                      //RIGHT
                     else if(j==2 && axis==0 && direction==1) 
                     {
-                      newPlane.setAttribute('material','shader:flat ; src: url(../../../assets/texture/tileA_'+r+'_'+i+'.png)');
+                      newPlane.setAttribute('material','shader:flat ; src: url(../../../assets/texture/tileA_'+(2-i)+'_'+r+'.png)');
                       rotation = "0 90 90";
                     }
 
