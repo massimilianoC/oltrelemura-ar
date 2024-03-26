574a3e6d72c3b6780f3bc7e26ebdd813196b0e5b
massimilianoC <massimiliano.camillucci@gmail.com>
2024-03-26 09:18:51 +0100

    rot

--- scripts/rubik-cube.js
+++ scripts/rubik-cube.js
@@ -52,7 +52,7 @@ AFRAME.registerComponent('rubik-cube',{
                     else if(j==0 && axis==0 && direction==0) 
                     {
                       newPlane.setAttribute('material','shader:flat ; src: url(../../../assets/texture/tileA_'+r+'_'+i+'.png)');
-                      rotation = "0 90 90";
+                      rotation = "0 90 -90";
                     }
 /*
                     //RIGHT
