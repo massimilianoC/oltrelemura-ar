1ca897ac5cccb82e60d9d8d0c5490ae36762007a
massimilianoC <massimiliano.camillucci@gmail.com>
2024-03-26 09:11:09 +0100

    i=2

--- scripts/rubik-cube.js
+++ scripts/rubik-cube.js
@@ -40,7 +40,7 @@ AFRAME.registerComponent('rubik-cube',{
                     
                     
                     //UP
-                    if(i==0 && axis==1 && direction==0) 
+                    if(i==2 && axis==1 && direction==0) 
                     {
                       newPlane.setAttribute('material','shader:flat ; src: url(../../../assets/texture/tileA_'+(2-r)+'_'+j+'.png)');
                     }
