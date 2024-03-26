deebe8735024575194c40499e80ff70bd949b76c
massimilianoC <massimiliano.camillucci@gmail.com>
2024-03-26 09:13:32 +0100

    down

--- scripts/rubik-cube.js
+++ scripts/rubik-cube.js
@@ -45,6 +45,13 @@ AFRAME.registerComponent('rubik-cube',{
                       newPlane.setAttribute('material','shader:flat ; src: url(../../../assets/texture/tileA_'+r+'_'+j+'.png)');
                     }
 
+                     //DOWN
+                     if(i==0 && axis==1 && direction==1) 
+                     {
+                       newPlane.setAttribute('material','shader:flat ; src: url(../../../assets/texture/tileA_'+r+'_'+j+'.png)');
+                     }
+ 
+
                     /*
                     //LEFT
                     if(j==0 && axis==1 && direction==1) 
