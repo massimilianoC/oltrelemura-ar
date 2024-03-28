04f3ea7ec5fb2f08de089ae0f6dca5cad3a0d624
massimilianoC <massimiliano.camillucci@gmail.com>
2024-03-28 12:13:23 +0100

    debug

--- scripts/rubik-cube.js
+++ scripts/rubik-cube.js
@@ -41,6 +41,7 @@ function buildCubeFaces(el){
                   newPivot.setAttribute('id','pivot_'+r+'_'+i+'_'+j);
                   newBlock.setAttribute("face","");
                   newBlock.setAttribute("faceDir",{x:0,y:0,z:0});
+                  console.log(newBlock.getAttribute("faceDir"));
                   newBlock.setAttribute('id','block_'+r+'_'+i+'_'+j);
                   newBlock.setAttribute('class',"block");
                   newBlock.setAttribute('position',positionx+" "+positiony+ " "+positionz);
